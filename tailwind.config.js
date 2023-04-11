f3dedf9df43b6ad4c592f6668c4ce28c4eda5d23
Enoch Ansah <enocansah@yahoo.com>
2023-04-11 01:11:11 +0000

    Milestone: Showcase, Services, Toolbox and Contact UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'mobile2': '434px', 
+      
+      'mobile': '561px',
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+      
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'md2': '905px',
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'lg2': '1180px',
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   plugins: [],
